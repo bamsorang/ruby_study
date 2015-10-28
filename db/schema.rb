58d6cb6cc47a6e8888e1feca410310fd4c3e298a
Nifer(Soyoung Park) <nifer.p@daumkakao.com>
2015-10-28 17:36:39 +0900

    bin/rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151027024523) do
+ActiveRecord::Schema.define(version: 20151028083359) do
 
   create_table "bulletins", force: :cascade do |t|
     t.string   "title"
@@ -23,8 +23,11 @@ ActiveRecord::Schema.define(version: 20151027024523) do
   create_table "posts", force: :cascade do |t|
     t.string   "title"
     t.text     "content"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.integer  "bulletin_id"
   end
+
+  add_index "posts", ["bulletin_id"], name: "index_posts_on_bulletin_id"
 
 end
